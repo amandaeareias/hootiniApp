99e6a724c3ceb70f6b0b79adac03ec956e332a62
Amanda Areias <amanda.1108@hotmail.com>
2019-03-08 11:31:24 +0100

    created landing page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,8 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Button,
+  ImageBackground
 } from 'react-native';
 import { WebBrowser } from 'expo';
 
@@ -19,48 +21,31 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
+      <ImageBackground source={require('../assets/images/doodles.png')} style={{ width: '100%', height: '100%', resizeMode: 'repeat' }}>
+        <Image source={require('../assets/images/logo.png')} style={{
+          marginTop: 30,
+          marginLeft: 30
+        }} />
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+        >
+          <Text>Remember anything, forever.</Text>
+          <Button
+            onPress={console.log('button clicked')}
+            title="GET STARTED FOR FREE"
+            color="#841584"
+          />
+          <Button
+            onPress={console.log('button clicked')}
+            title="I ALREADY HAVE AN ACCOUNT"
+            color="#841584"
+          />
         </View>
-      </View>
+      </ImageBackground>
     );
   }
 
