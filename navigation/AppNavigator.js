0267eb0cebc2f563c175a7413f58fd08ea6038ea
Amanda Areias <amanda.1108@hotmail.com>
2019-03-08 12:14:53 +0100

    working on navigation

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import SignUp from './SignUp'
+import SignIn from './SignIn'
+
+import { createStackNavigator } from 'react-navigation'
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
+  SignUp: { screen: SignUp },
+  SignIn: { screen: SignIn }
 }));
+
+// const AppNavigator = createStackNavigator({
+
+//     Home: HomeScreen,
+//     SignUp: { screen: SignUp },
+//     SignIn: { screen: SignIn }
+//   },
+//   {
+//     initialRouteName: 'Home'
+
+// })
+
+// export default AppNavigator;
---
+++ screens/SignIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text } from 'react-native';
+
+export default class SignIn extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <Text>SignIn works!</Text>
+    )
+  }
+}
