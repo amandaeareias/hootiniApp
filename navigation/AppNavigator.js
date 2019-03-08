6db65350f384d3ab20c5ee616278de39d3040dc6
Sue Li <sue.li@post.harvard.edu>
2019-03-08 21:34:49 +0100

    feat: logout works

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -10,9 +10,12 @@ export default createAppContainer(createStackNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   // Main: MainTabNavigator,
-  // SignUp: { screen: SignUp },
-  // SignIn: { screen: SignIn },
-  // Home: { screen: HomeScreen }
-  Decks: { screen: Decks }
+  SignUp: { screen: SignUp },
+  SignIn: { screen: SignIn },
+  Home: { screen: HomeScreen },
+  Decks: { screen: Decks },
+  headerMode: 'screen'
 }));
+
+
 
--- screens/Decks.js
+++ screens/Decks.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import {Text } from 'react-native';
+import React, { Component } from 'react';
+import { Text, Button } from 'react-native';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default class Decks extends React.Component {
-  static navigationOptions = {
+
+const SIGNOUT_MUTATION = gql`
+  mutation signout {
+    signout {
+      message
+    }
+  }
+`;
+
+export default class Decks extends Component {
+   
+  static navigationOptions = ({navigation}) => {
+    return {
     title: 'My Decks',
+    headerRight: <Mutation mutation={SIGNOUT_MUTATION}>{(signout) => (
+      <Button title="Log out" onPress={() => {
+        handleClick(signout);
+        navigation.navigate('Home')
+        }}/>
+    )}</Mutation>
+    }
   };
 
   render() {
@@ -12,3 +32,10 @@ export default class Decks extends React.Component {
     )
   }
 }
+
+handleClick = async (signout) => {
+  await signout();
+  // await client.resetStore();
+} 
+
+
