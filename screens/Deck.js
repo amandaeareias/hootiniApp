a254e0e81487169cc0895a64304b512c07144579
Sue Li <sue.li@post.harvard.edu>
2019-03-09 19:02:21 +0100

    feat: loading notes on decks page

--- screens/Deck.js
+++ screens/Deck.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Text, Button, ScrollView, Modal, TextInput } from 'react-native';
 import { Mutation, Query, renderToStringWithData } from 'react-apollo';
 import gql from 'graphql-tag';
-import User from '../components/User'
-import DeckList from '../components/DeckList'
+import User from '../components/User';
 
 const DECK_QUERY = gql`
   query Deck($slug: String!) {
@@ -17,6 +16,22 @@ const DECK_QUERY = gql`
         id
         slug
         name
+      }
+    }
+  }
+`;
+
+const DUE_CARDS_QUERY = gql`
+  query allCards($deckSlug: String!, $when: DateTime!) {
+    allCards(where: { deckSlug: $deckSlug, dueTime_lt: $when }) {
+      id
+      fields {
+        key
+        value
+      }
+      template {
+        front
+        back
       }
     }
   }
@@ -58,6 +73,29 @@ class Deck extends Component {
                   <Text>
                   {deck.name}
                   </Text>
+
+                  <ScrollView>
+
+                  <Query query={DUE_CARDS_QUERY} variables={{ deckSlug: slug, when: Date.now() }}>
+                    {({ data, error, loading }) => {
+                      if (loading) {
+                        return <Text>Loading...</Text>
+                      }
+                      if (error) {
+                        return <p>Error! {error.message}</p>;
+                      }
+                      const { allCards } = data;
+
+                      const cards = allCards.map(note => {
+                        console.log(note);
+                        return <Text key={note.id} style={{margin: 10, width: 50, height: 50, fontSize: 10, padding: 10, backgroundColor: '#DEDEDE'}}>{note.fields[0].value}</Text>;
+                      });
+                      return cards;
+                    }}
+                  </Query>
+
+                  </ScrollView>
+
                   <Button title="Add New Note" onPress={() => this.props.navigation.navigate('AddNote', { deck: deck})}/>
                 </ScrollView>
                 } else {
